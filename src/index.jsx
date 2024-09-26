89be72191c7c624767d71e3d95e91e3879dd8652
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-09-25 19:52:22 -0500

    initial commit after initializing parcel and npm successfully

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const MyFlixApp = () => {
+  return <h1>Welcome to myFlix!</h1>;
+};
+
+ReactDOM.render(<MyFlixApp />, document.getElementById('app'));
