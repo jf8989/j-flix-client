b1946e7dd984912e46002b849e3af64a4f832dae
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2024-10-13 17:06:45 -0500

    improved footer UI

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,54 +6,46 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <Row className="social-icons">
-          <Col>
-            <FaFacebookF />
-            <FaInstagram />
-            <FaTwitter />
-            <FaYoutube />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={3}>
+        <div className="social-icons">
+          <FaFacebookF />
+          <FaInstagram />
+          <FaTwitter />
+          <FaYoutube />
+        </div>
+        <div className="footer-links">
+          <div className="col">
             <ul>
               <li>Audio Description</li>
               <li>Investor Relations</li>
               <li>Legal Notices</li>
             </ul>
-          </Col>
-          <Col md={3}>
+          </div>
+          <div className="col">
             <ul>
               <li>Help Center</li>
               <li>Jobs</li>
               <li>Cookie Preferences</li>
             </ul>
-          </Col>
-          <Col md={3}>
+          </div>
+          <div className="col">
             <ul>
               <li>Gift Cards</li>
               <li>Terms of Use</li>
               <li>Corporate Information</li>
             </ul>
-          </Col>
-          <Col md={3}>
+          </div>
+          <div className="col">
             <ul>
               <li>Media Center</li>
               <li>Privacy</li>
               <li>Contact Us</li>
             </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <button className="service-code">Service Code</button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p className="copyright">&copy; 1997-2024 J-Flix, Inc.</p>
-          </Col>
-        </Row>
+          </div>
+        </div>
+        <div className="text-center">
+          <button className="service-code">Service Code</button>
+        </div>
+        <p className="copyright">&copy; 2024 J-Flix, Inc.</p>
       </Container>
     </footer>
   );
