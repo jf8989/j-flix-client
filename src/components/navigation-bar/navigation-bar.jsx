84dbe7198c135e8c0dd7dbceb962beaa46efc0ff
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-10-14 17:29:04 -0500

    - added a more complex profile view solution

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,129 +1,126 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { Navbar, Container, Nav, Form } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
+import { Navbar, Container, Nav, Form, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FaSearch, FaBell } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { setFilter } from "../../redux/moviesSlice";
-import { FaSearch, FaBell, FaSignOutAlt, FaUser } from "react-icons/fa";
+import profilePic from "../../assets/images/profilepic.jpg";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [showSearch, setShowSearch] = useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchValue, setSearchValue] = useState("");
-
+  const searchInputRef = useRef(null);
   const dispatch = useDispatch();
 
-  const handleFilterChange = useCallback(
-    (e) => {
-      setSearchValue(e.target.value);
-      dispatch(setFilter(e.target.value));
-    },
-    [dispatch]
-  );
+  useEffect(() => {
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
+    if (isSearchOpen && searchInputRef.current) {
+      searchInputRef.current.focus();
+    }
+  }, [isSearchOpen]);
 
-    const handleClickOutside = (event) => {
-      if (
-        showSearch &&
-        !event.target.closest(".navbar .form-control") &&
-        !event.target.closest(".navbar .fa-search")
-      ) {
-        setShowSearch(false);
-        setSearchValue("");
-        dispatch(setFilter(""));
-      }
-    };
+  const handleSearchToggle = () => {
+    setIsSearchOpen(!isSearchOpen);
+    if (!isSearchOpen) {
+      setSearchValue("");
+      dispatch(setFilter(""));
+    }
+  };
 
-    window.addEventListener("scroll", handleScroll);
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [showSearch, dispatch]);
+  const handleSearchChange = (e) => {
+    const value = e.target.value;
+    setSearchValue(value);
+    dispatch(setFilter(value));
+  };
 
   return (
     <Navbar
-      bg="dark"
       variant="dark"
       expand="lg"
-      className={`${isScrolled ? "navbar-scrolled" : ""}`}
+      className={`styled-navbar ${isScrolled ? "scrolled" : ""}`}
       fixed="top"
     >
       <Container fluid>
-        <Navbar.Brand as={Link} to="/">
-          <span style={{ color: "var(--primary-color)", fontWeight: "bold" }}>
-            j-Flix
-          </span>
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          style={{ color: "#e50914", fontWeight: "bold" }}
+        >
+          j-Flix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {user && (
-              <>
-                <Nav.Link as={Link} to="/">
-                  Home
-                </Nav.Link>
-                <Nav.Link as={Link} to="/tvshows">
-                  TV Shows
-                </Nav.Link>
-                <Nav.Link as={Link} to="/movies">
-                  Movies
-                </Nav.Link>
-                <Nav.Link as={Link} to="/new">
-                  New & Popular
-                </Nav.Link>
-                <Nav.Link as={Link} to="/mylist">
-                  My List
-                </Nav.Link>
-              </>
-            )}
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/tvshows">
+              TV Shows
+            </Nav.Link>
+            <Nav.Link as={Link} to="/movies">
+              Movies
+            </Nav.Link>
+            <Nav.Link as={Link} to="/new">
+              New & Popular
+            </Nav.Link>
+            <Nav.Link as={Link} to="/mylist">
+              My List
+            </Nav.Link>
           </Nav>
           <Nav>
-            {!user && (
-              <>
-                <Nav.Link as={Link} to="/login">
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to="/signup">
-                  Signup
-                </Nav.Link>
-              </>
-            )}
-            {user && (
-              <>
-                <Nav.Link
-                  onClick={() => setShowSearch(!showSearch)}
-                  title="Search"
-                >
-                  <FaSearch />
-                </Nav.Link>
-                {showSearch && (
-                  <Form className="d-flex show-search">
-                    <Form.Control
-                      type="text"
-                      placeholder="Search movies..."
-                      value={searchValue}
-                      onChange={handleFilterChange}
-                      className="me-2"
-                    />
-                  </Form>
-                )}
-                <Nav.Link href="#" title="Notifications">
-                  <FaBell />
-                </Nav.Link>
-                <Nav.Link as={Link} to="/profile" title="Profile">
-                  <FaUser />
-                </Nav.Link>
-                <Nav.Link onClick={onLoggedOut} title="Sign out">
-                  <FaSignOutAlt />
-                </Nav.Link>
-              </>
-            )}
+            <div className="search-container">
+              <FaSearch
+                onClick={handleSearchToggle}
+                style={{ cursor: "pointer", marginRight: "20px" }}
+              />
+              <Form.Control
+                ref={searchInputRef}
+                type="text"
+                placeholder="Titles, people, genres"
+                value={searchValue}
+                onChange={handleSearchChange}
+                className={`search-input ${isSearchOpen ? "open" : ""}`}
+              />
+            </div>
+            <Nav.Link as={Link} to="/kids">
+              Kids
+            </Nav.Link>
+            <Nav.Link href="#" title="Notifications">
+              <FaBell />
+            </Nav.Link>
+            <Dropdown align="end" className="profile-dropdown">
+              <Dropdown.Toggle as={Nav.Link}>
+                <img src={profilePic} alt="Profile" className="profile-image" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item as={Link} to="/profile">
+                  Profile
+                </Dropdown.Item>
+                <Dropdown.Item>Profile 2</Dropdown.Item>
+                <Dropdown.Item>Profile 3</Dropdown.Item>
+                <Dropdown.Item>Profile 4</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item as={Link} to="/manage-profiles">
+                  Manage Profiles
+                </Dropdown.Item>
+                <Dropdown.Item as={Link} to="/account">
+                  Account
+                </Dropdown.Item>
+                <Dropdown.Item as={Link} to="/help">
+                  Help Center
+                </Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item onClick={onLoggedOut}>
+                  Sign out of Netflix
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
