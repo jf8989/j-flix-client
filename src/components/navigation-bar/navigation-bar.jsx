7f9e5a7293c1109126ce231a4b0a0ff01684c919
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-10-14 20:12:14 -0500

    fixed some nav issues

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -11,7 +11,11 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchValue, setSearchValue] = useState("");
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMobileProfileMenuOpen, setIsMobileProfileMenuOpen] = useState(false);
   const searchInputRef = useRef(null);
+  const navbarRef = useRef(null);
+  const mobileProfileMenuRef = useRef(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,12 +25,28 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
   }, []);
 
   useEffect(() => {
-    if (isSearchOpen && searchInputRef.current) {
-      searchInputRef.current.focus();
-    }
-  }, [isSearchOpen]);
+    const handleClickOutside = (event) => {
+      if (
+        navbarRef.current &&
+        !navbarRef.current.contains(event.target) &&
+        mobileProfileMenuRef.current &&
+        !mobileProfileMenuRef.current.contains(event.target)
+      ) {
+        setIsSearchOpen(false);
+        setIsProfileMenuOpen(false);
+        setIsMobileMenuOpen(false);
+        setIsMobileProfileMenuOpen(false);
+      }
+    };
 
-  const handleSearchToggle = () => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleSearchToggle = (e) => {
+    e.stopPropagation();
     setIsSearchOpen(!isSearchOpen);
     if (!isSearchOpen) {
       setSearchValue("");
@@ -40,12 +60,17 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
     dispatch(setFilter(value));
   };
 
-  const handleProfileMouseEnter = () => {
-    setIsProfileMenuOpen(true);
+  const handleProfileMenuToggle = (isOpen) => {
+    setIsProfileMenuOpen(isOpen);
   };
 
-  const handleProfileMouseLeave = () => {
-    setIsProfileMenuOpen(false);
+  const handleMobileProfileMenuToggle = (e) => {
+    e.stopPropagation();
+    setIsMobileProfileMenuOpen(!isMobileProfileMenuOpen);
+  };
+
+  const handleMobileMenuToggle = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
@@ -54,91 +79,182 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
       expand="lg"
       className={`styled-navbar ${isScrolled ? "scrolled" : ""}`}
       fixed="top"
+      ref={navbarRef}
     >
-      <Container fluid>
+      <Container fluid className="nav-container">
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="order-0"
+          onClick={handleMobileMenuToggle}
+        />
         <Navbar.Brand
           as={Link}
           to="/"
+          className="order-1"
           style={{ color: "#e50914", fontWeight: "bold" }}
         >
           j-Flix
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={`order-2 ${isMobileMenuOpen ? "show" : ""}`}
+        >
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
+            <Nav.Link
+              as={Link}
+              to="/"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to="/tvshows">
+            <Nav.Link
+              as={Link}
+              to="/tvshows"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               TV Shows
             </Nav.Link>
-            <Nav.Link as={Link} to="/movies">
+            <Nav.Link
+              as={Link}
+              to="/movies"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               Movies
             </Nav.Link>
-            <Nav.Link as={Link} to="/new">
+            <Nav.Link
+              as={Link}
+              to="/new"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               New & Popular
             </Nav.Link>
-            <Nav.Link as={Link} to="/mylist">
+            <Nav.Link
+              as={Link}
+              to="/mylist"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
               My List
             </Nav.Link>
           </Nav>
-          <Nav>
-            <div className="search-container">
-              <FaSearch
-                onClick={handleSearchToggle}
-                style={{ cursor: "pointer", marginRight: "20px" }}
-              />
-              <Form.Control
-                ref={searchInputRef}
-                type="text"
-                placeholder="Titles, people, genres"
-                value={searchValue}
-                onChange={handleSearchChange}
-                className={`search-input ${isSearchOpen ? "open" : ""}`}
-              />
-            </div>
-            <Nav.Link as={Link} to="/kids">
-              Kids
-            </Nav.Link>
-            <Nav.Link href="#" title="Notifications">
-              <FaBell />
-            </Nav.Link>
-            <Dropdown
-              align="end"
-              className="profile-dropdown"
-              show={isProfileMenuOpen}
-              onMouseEnter={handleProfileMouseEnter}
-              onMouseLeave={handleProfileMouseLeave}
-            >
-              <Dropdown.Toggle as={Nav.Link}>
-                <img src={profilePic} alt="Profile" className="profile-image" />
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item as={Link} to="/profile">
-                  Profile
-                </Dropdown.Item>
-                <Dropdown.Item>Profile 2</Dropdown.Item>
-                <Dropdown.Item>Profile 3</Dropdown.Item>
-                <Dropdown.Item>Profile 4</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item as={Link} to="/manage-profiles">
-                  Manage Profiles
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/account">
-                  Account
-                </Dropdown.Item>
-                <Dropdown.Item as={Link} to="/help">
-                  Help Center
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item onClick={onLoggedOut}>
-                  Sign out of Netflix
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Nav>
         </Navbar.Collapse>
+        <Nav className="ml-auto order-3 d-none d-lg-flex">
+          <div className="search-container">
+            <FaSearch
+              onClick={handleSearchToggle}
+              style={{ cursor: "pointer", marginRight: "20px" }}
+            />
+            <Form.Control
+              ref={searchInputRef}
+              type="text"
+              placeholder="Titles, people, genres"
+              value={searchValue}
+              onChange={handleSearchChange}
+              className={`search-input ${isSearchOpen ? "open" : ""}`}
+            />
+          </div>
+          <Nav.Link as={Link} to="/kids">
+            Kids
+          </Nav.Link>
+          <Nav.Link href="#" title="Notifications">
+            <FaBell />
+          </Nav.Link>
+          <Dropdown
+            align="end"
+            className={`profile-dropdown ${isProfileMenuOpen ? "show" : ""}`}
+            show={isProfileMenuOpen}
+            onToggle={handleProfileMenuToggle}
+          >
+            <Dropdown.Toggle as={Nav.Link}>
+              <img src={profilePic} alt="Profile" className="profile-image" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to="/profile">
+                Profile
+              </Dropdown.Item>
+              <Dropdown.Item>Profile 2</Dropdown.Item>
+              <Dropdown.Item>Profile 3</Dropdown.Item>
+              <Dropdown.Item>Profile 4</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item as={Link} to="/manage-profiles">
+                Manage Profiles
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/account">
+                Account
+              </Dropdown.Item>
+              <Dropdown.Item as={Link} to="/help">
+                Help Center
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item onClick={onLoggedOut}>
+                Sign out of Netflix
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Nav>
+        <div className="mobile-icons order-4 d-flex d-lg-none">
+          <FaSearch onClick={handleSearchToggle} />
+          <FaBell />
+          <img
+            src={profilePic}
+            alt="Profile"
+            className="profile-image"
+            onClick={handleMobileProfileMenuToggle}
+          />
+        </div>
       </Container>
+      <Form
+        className={`mobile-search-form d-lg-none ${isSearchOpen ? "show" : ""}`}
+      >
+        <Form.Control
+          type="text"
+          placeholder="Titles, people, genres"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+      </Form>
+      <div
+        ref={mobileProfileMenuRef}
+        className={`mobile-profile-menu d-lg-none ${
+          isMobileProfileMenuOpen ? "show" : ""
+        }`}
+      >
+        <Nav.Link
+          as={Link}
+          to="/profile"
+          onClick={() => setIsMobileProfileMenuOpen(false)}
+        >
+          Profile
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/manage-profiles"
+          onClick={() => setIsMobileProfileMenuOpen(false)}
+        >
+          Manage Profiles
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/account"
+          onClick={() => setIsMobileProfileMenuOpen(false)}
+        >
+          Account
+        </Nav.Link>
+        <Nav.Link
+          as={Link}
+          to="/help"
+          onClick={() => setIsMobileProfileMenuOpen(false)}
+        >
+          Help Center
+        </Nav.Link>
+        <Nav.Link
+          onClick={() => {
+            onLoggedOut();
+            setIsMobileProfileMenuOpen(false);
+          }}
+        >
+          Sign out of Netflix
+        </Nav.Link>
+      </div>
     </Navbar>
   );
 };
