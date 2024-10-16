bad8f5cc77b065089ba4dd4d9de3024739e759f3
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-10-15 20:16:02 -0500

    improved movie card UI

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,34 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Star, StarFill } from "react-bootstrap-icons";
 import defaultPoster from "../../assets/images/default-movie-poster.jpg";
 
 export const MovieCard = ({ movie, onToggleFavorite, isFavorite }) => {
+  // Function to truncate description to two lines
+  const truncateDescription = (text, maxLength = 60) => {
+    if (text.length <= maxLength) return text;
+    return text.substr(0, text.lastIndexOf(" ", maxLength)) + "...";
+  };
+
   return (
-    <Card className="h-100 bg-dark text-white d-flex flex-column">
-      <Card.Img variant="top" src={defaultPoster} alt={movie.title} />
+    <Card className="h-100 bg-dark text-white d-flex flex-column movie-card">
+      <Link to={`/movies/${movie._id}`}>
+        <Card.Img variant="top" src={defaultPoster} alt={movie.title} />
+      </Link>
       <Card.Body className="d-flex flex-column">
-        <Card.Title className="text-truncate">{movie.title}</Card.Title>
+        <Link
+          to={`/movies/${movie._id}`}
+          className="text-white text-decoration-none"
+        >
+          <Card.Title className="text-truncate">{movie.title}</Card.Title>
+        </Link>
         <Card.Text className="movie-description flex-grow-1 small">
-          {movie.description.length > 100
-            ? `${movie.description.substring(0, 100)}...`
-            : movie.description}
+          {truncateDescription(movie.description)}
         </Card.Text>
-        <div className="mt-auto">
-          <Link to={`/movies/${movie._id}`} className="d-grid mb-2">
-            <Button variant="outline-light" size="sm">
-              Open
-            </Button>
-          </Link>
-          <Button
-            variant={isFavorite ? "danger" : "outline-light"}
-            onClick={() => onToggleFavorite(movie._id)}
-            className="w-100"
-            size="sm"
+        <div className="mt-auto d-flex justify-content-end">
+          <span
+            onClick={(e) => {
+              e.preventDefault();
+              onToggleFavorite(movie._id);
+            }}
+            style={{ cursor: "pointer" }}
           >
-            {isFavorite ? "Remove" : "Add to Favorites"}
-          </Button>
+            {isFavorite ? (
+              <StarFill color="gold" size={24} />
+            ) : (
+              <Star color="white" size={24} />
+            )}
+          </span>
         </div>
       </Card.Body>
     </Card>
