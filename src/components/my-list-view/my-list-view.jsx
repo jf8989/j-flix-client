24ad0f16989e82f5438b2d0dd7f65c8a42e99ff2
Juan Francisco Marcenaro Arellano <128626952+jf8989@users.noreply.github.com>
2024-11-16 21:03:09 -0500

    adjusted the mobile view for my list

--- src/components/my-list-view/my-list-view.jsx
+++ src/components/my-list-view/my-list-view.jsx
@@ -3,46 +3,44 @@ import { Row, Col, Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { MovieCard } from "../movie-card/movie-card";
 import { BackArrow } from "../back-arrow/back-arrow";
+import "./my-list-view.scss";
 
 export const MyListView = ({ user, movies, onToggleFavorite }) => {
   const favoriteMovies = user?.FavoriteMovies || [];
   const filter = useSelector((state) => state.movies.filter);
 
+  const filteredMovies = movies
+    .filter((movie) => favoriteMovies.includes(movie._id))
+    .filter((movie) =>
+      movie.title.toLowerCase().includes(filter.toLowerCase())
+    );
+
   return (
-    <Container fluid className="p-3 content-margin">
-      <Row className="align-items-center mb-4">
-        <Col xs="auto">
-          <BackArrow className="mt-1" />
-        </Col>
-        <Col>
-          <h2 className="text-white mb-0">Watch List</h2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          {movies.length > 0 && favoriteMovies.length > 0 ? (
-            <Row xs={1} sm={2} md={3} lg={4} xl={5} className="g-4">
-              {movies
-                .filter((movie) => favoriteMovies.includes(movie._id))
-                .filter((movie) =>
-                  movie.title.toLowerCase().includes(filter.toLowerCase())
-                )
-                .map((movie) => (
-                  <Col key={movie._id}>
-                    <MovieCard
-                      movie={movie}
-                      onToggleFavorite={onToggleFavorite}
-                      isFavorite={true}
-                    />
-                  </Col>
-                ))}
-            </Row>
-          ) : (
-            <p className="text-white">No movies in your list yet.</p>
-          )}
-        </Col>
-      </Row>
-    </Container>
+    <div className="my-list-view">
+      <Container fluid className="p-3 content-margin">
+        <div className="list-header">
+          <BackArrow className="back-arrow" />
+          <h2>Watch List</h2>
+        </div>
+
+        {filteredMovies.length > 0 ? (
+          <div className="movies-grid">
+            {filteredMovies.map((movie) => (
+              <MovieCard
+                key={movie._id}
+                movie={movie}
+                onToggleFavorite={onToggleFavorite}
+                isFavorite={true}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="empty-state">
+            <p>No movies in your list yet.</p>
+          </div>
+        )}
+      </Container>
+    </div>
   );
 };
 
