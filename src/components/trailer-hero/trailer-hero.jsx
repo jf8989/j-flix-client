b2c7c235c87bb086403ffbff9a014fcfbb69d80c
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-12-16 15:19:20 -0500

    made full screen button visible on any screen size

--- src/components/trailer-hero/trailer-hero.jsx
+++ src/components/trailer-hero/trailer-hero.jsx
@@ -305,20 +305,19 @@ const TrailerHero = ({ movies, series }) => {
             >
               {isMuted ? <FaVolumeMute /> : <FaVolumeUp />}
             </button>
-            {window.innerWidth <= 768 && (
-              <button
-                className="control-button fullscreen-button-mobile"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleDoubleClick();
-                }}
-                aria-label="Toggle Fullscreen"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                  <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" />
-                </svg>
-              </button>
-            )}
+
+            <button
+              className="control-button fullscreen-button-mobile"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleDoubleClick();
+              }}
+              aria-label="Toggle Fullscreen"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
